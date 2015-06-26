f0f3aa0ad80186c6377d1dc5af0f7ffd3a448f04
Robert Deutz <rdeutz@googlemail.com>
2015-06-26 15:34:11 +0200

    new dashboard

--- code/administrator/components/com_babioonevent/views/cpanels/tmpl/default.php
+++ code/administrator/components/com_babioonevent/views/cpanels/tmpl/default.php
@@ -1,31 +1,35 @@
 <?php
+// No direct access
+defined('_JEXEC') or die;
+$sidebar = JHtmlSidebar::render();
+?>
+<div id="babioonevent">
+<?php if (!empty( $sidebar)) : ?>
+	<div id="j-sidebar-container" class="span2">
+		<?php echo $sidebar; ?>
+	</div>
+	<div id="j-main-container" class="span10 well">
+<?php else : ?>
+	<div id="j-main-container well">
+<?php endif;?>
+		<?php echo JText::_('COM_BABIOONEVENT_COMP');?>
+		<?php echo JText::_('COM_BABIOONEVENT_COMP_DESC');?>
 
-JFactory::getDocument()->addStyleDeclaration('
-	.componentname {height: 60px}
-	.componentname img {vertical-align: top;}
-	.componentname p {padding-top:10px;font-size:2em;line-height:1.2em;margin-left:10px;display:inline-block;}
-');
+		<div class="cpanel" style="padding:20px">
 
+			<?php echo JText::_('COM_BABIOONEVENT_COMP_SUPPORT');?>
+			<?php echo JText::_('COM_BABIOONEVENT_COMP_DOCS');?>
+			<?php echo JText::_('COM_BABIOONEVENT_COMP_FORUM');?>
 
-if (JFactory::getUser()->authorise('core.admin', 'com_babioonevent'))
-{
-	echo JText::_('COM_BABIOONEVENT_CONFHERE_IMG');
-}
-echo JText::_('COM_BABIOONEVENT_COMP');
-echo JText::_('COM_BABIOONEVENT_COMP_DESC');
+			<?php if (JFactory::getUser()->authorise('core.admin', 'com_babioonevent')) : ?>
+				<?php echo LiveUpdate::getIcon(); ?>
+			<?php endif;?>
 
-echo '<div class="cpanel" style="padding:20px">';
-echo JText::_('COM_BABIOONEVENT_COMP_SUPPORT');
-echo JText::_('COM_BABIOONEVENT_COMP_DOCS');
-echo JText::_('COM_BABIOONEVENT_COMP_FORUM');
-if (JFactory::getUser()->authorise('core.admin', 'com_babioonevent'))
-{
-	echo LiveUpdate::getIcon();
-}
+		</div>
+		<div class="clearfix"> </div>
 
-
-echo '</div>';
-echo '<div class="clr"></div>';
-
-echo '<p>' . JText::_('COM_BABIOONEVENT_COPYRIGHT') . ' | ';
-echo JText::_('COM_BABIOONEVENT_LICENSE') . '</p>';
+		<p>
+			<?php echo JText::_('COM_BABIOONEVENT_COPYRIGHT') . ' | ' . JText::_('COM_BABIOONEVENT_LICENSE'); ?>
+		</p>
+	</div>
+</div>
